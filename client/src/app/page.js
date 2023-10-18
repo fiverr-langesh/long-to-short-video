56386f51faa974063c4eda8ef86538df6922d2e4
Nithishpandian <nithishpandiyan75@gmail.com>
2023-10-18 15:02:23 +0530

    completed the video section

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -1,9 +1,11 @@
 import Navbar from '@/components/common/Navbar'
+import VideoContainer from '@/components/video/VideoContainer'
 
 export default function Home() {
   return (
     <div className=' py-8 px-16 bg-slate-900'>
       <Navbar />
+      <VideoContainer />
     </div>
   )
 }
