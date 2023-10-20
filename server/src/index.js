4310fd7c4ca11bc0e577062b0e82891ae0a3f7dd
langesh <dev.langesh@gmail.com>
2023-10-20 08:52:08 +0530

    webhook

--- server/src/index.js
+++ server/src/index.js
@@ -20,15 +20,40 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-// (async () => {
-//   const intent = await stripe.paymentIntents.create({
-//     amount: 1000, // amount in cents
-//     currency: "usd",
-//     // Add additional payment intent parameters as needed
-//   });
+const endpointSecret =
+  "whsec_fa606cc8c0a116ad23ed490c6ef869908e404a7d8696965e0bd57bf7c99bc83a";
+  
+app.post(
+  "/webhook",
+  express.raw({ type: "application/json" }),
+  (request, response) => {
+    const sig = request.headers["stripe-signature"];
 
-//   console.log(intent.client_secret);
-// })()
+    let event;
+
+    try {
+      event = stripe.webhooks.constructEvent(request.body, sig, endpointSecret);
+    } catch (err) {
+      response.status(400).send(`Webhook Error: ${err.message}`);
+      return;
+    }
+
+    // Handle the event
+    switch (event.type) {
+      case "payment_intent.succeeded":
+        const paymentIntentSucceeded = event.data.object;
+        // Then define and call a function to handle the event payment_intent.succeeded
+        break;
+      // ... handle other event types
+      default:
+        console.log(`Unhandled event type ${event.type}`);
+    }
+
+    // Return a 200 response to acknowledge receipt of the event
+    response.send();
+  }
+);
+
 
 app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`);
