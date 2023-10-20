8edbaa9330ac623d98506ed853788be092b547b9
Nithishpandian <nithishpandiyan75@gmail.com>
2023-10-20 19:09:56 +0530

    completed the video UI

--- client/src/components/home/HomeSection.jsx
+++ client/src/components/home/HomeSection.jsx
@@ -1,14 +1,21 @@
-import React from 'react'
+"use client"
+import React, { useState } from 'react'
 import InputComponent from './InputComponent'
 import File from './File'
+import VideoLoad from './VideoLoad'
 
 function HomeSection() {
+  const [fileChoosed, setFileChoosed] = useState(true)
   return (
     <div className=' flex flex-col justify-center items-center gap-14 my-52'>
         <div className="flex justify-center">
           <InputComponent />
         </div>
-        <File />
+        {
+          fileChoosed ?
+          <VideoLoad /> :
+          <File />
+        }
     </div>
   )
 }
