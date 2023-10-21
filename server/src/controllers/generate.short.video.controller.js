c48eccad7ff6eb678930f5bcdd431bdb67dabb03
langesh <dev.langesh@gmail.com>
2023-10-21 12:36:38 +0530

    output video

--- server/src/controllers/generate.short.video.controller.js
+++ server/src/controllers/generate.short.video.controller.js
@@ -6,25 +6,40 @@ async function generateShortVideo(req, res) {
   try {
     const { url, email } = req.body;
 
-      const user = await User.findOne({email}).lean();
-      
+    const user = await User.findOne({ email }).lean();
+
     if (!user) {
       return res.status(404).json({ message: "User not found" });
     }
 
     const userId = user._id;
 
-      const video = await Video.create({ url, userId });
-      
-      const aiReq = await axios.post('http://localhost:5000/ai', { url, user_id: userId });
-      
-      const { data } = aiReq;
-      
-      console.log(data)
+    const video = await Video.create({ url, userId });
 
-    return res.status(201).json({ message: "Video created" ,video});
+    const aiReq = await axios.post("http://localhost:5000/ai", {
+      url,
+      user_id: userId,
+    });
+
+    const { data } = aiReq;
+
+    console.log(data);
+
+    let updatedVideo;
+
+    if (aiReq.status === 200) {
+      updatedVideo = await Video.findByIdAndUpdate(
+        video._id,
+        { outputUrls: data.output_urls },
+        { new: true }
+      );
+
+      console.log(updatedVideo);
+    }
+
+    return res.status(201).json({ message: "Video created", video: updatedVideo });
   } catch (error) {
-      console.log(error)
+    console.log(error);
     return res.status(500).json({ message: "Internal server error" });
   }
 }
