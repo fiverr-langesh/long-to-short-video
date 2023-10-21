5db602e6d02842ba19027a18ec6fb70b481c232b
langesh <dev.langesh@gmail.com>
2023-10-21 12:36:19 +0530

    update video model with output url

--- server/src/model/video.model.js
+++ server/src/model/video.model.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const VideoSchema = mongoose.Schema({
     url: String,
     userId: String,
+    outputUrls: [String],
 }, {
     timestamps: true,
     collection: 'videos'
