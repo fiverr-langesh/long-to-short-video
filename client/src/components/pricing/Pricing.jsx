260dc9b6a64ddfc80f2028050426231b09473be1
langesh <dev.langesh@gmail.com>
2023-10-23 08:40:11 +0530

    updated pricing plan cost

--- client/src/components/pricing/Pricing.jsx
+++ client/src/components/pricing/Pricing.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { TiTick } from "react-icons/ti" 
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
@@ -7,6 +7,11 @@ import SubscribeButton from '../stripe/SubscribeButton';
 
 function Pricing({ mostPopular, type, price, subscription, optionTitle, option, option1, option2, option3, include2, include3, include5, include6 }) {
   const [options, setOptions] = useState(option1);
+  const [calcPrice, setCalcPrice] = useState(0);
+
+  useEffect(() => {
+    setCalcPrice(Number((price / option1) * options).toFixed(2));
+  },[options])
 
   const handleChange = (event) => {
     setOptions(event.target.value);
@@ -16,10 +21,10 @@ function Pricing({ mostPopular, type, price, subscription, optionTitle, option, 
     <div className={`${mostPopular && "bg-slate-800"} flex flex-col justify-center gap-4 px-10 py-8 rounded`}>
       <div className={` ${mostPopular ? "block" : "hidden"} text-xs font-medium bg-slate-700 text-slate-50 rounded-full py-0.5 px-2 w-fit`}>Most popular</div>
       <h1 className=' font-semibold text-xl text-white mb-7'>{type}</h1>
-      <h1 className=' font-bold text-4xl text-white'>$ {price}</h1>
+      {type !== "Free" ? <h1 className=' font-bold text-4xl text-white'>$ {calcPrice}</h1>: <h1 className=' font-bold text-4xl text-white'>$ 0</h1>}
       {/* <h1 className=' font-bold text-4xl text-white'>$ {Number(options * price).toFixed(2)}</h1> */}
 
-      <SubscribeButton display={subscription} amount={price} plan={type} minutes={options} />
+      <SubscribeButton display={subscription} amount={calcPrice} plan={type} minutes={options} />
 
       <p className=' font-medium text-slate-100 -mb-3'>{optionTitle}</p>
       <div className={`${option ? "block" : "hidden"}`}>
